e71130ec28c7d7c6db47110f4e872cb5e384925a
neyser zana <neyser@neysers-MacBook-Pro.local>
2018-11-14 16:23:06 -0500

    setting up our react app so we can start to render things

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Hello Gossamer</h1>
+        </div>
+    )
+}
+
+export default App;
---
+++ src/devtools/devtools.js
@@ -0,0 +1,6 @@
+chrome.devtools.panels.create(
+  'Lucid', // title for the panel tab
+  null, // you can specify here path to an icon
+  'indexTest.html', // html page which is gonna be injecting into the tab's content
+  null // you can pass here a callback function
+);
