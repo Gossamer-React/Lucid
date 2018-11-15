4aad6ead03d3f0299e7e933245ac6ad894e0f276
yongnicholaskim <yongnicholaskim@gmail.com>
2018-11-15 17:08:24 -0500

    get schema not working yet

--- src/containers/Effects.js
+++ src/containers/Effects.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react'
-import fetchSchema from 'fetch-graphql-schema';
+const {
+  introspect,
+  introspectURL,
+  introspectFile
+} = require('graphql-introspect')
+
+// import fetchSchema from 'fetch-graphql-schema';
+// import { HttpLink } from 'apollo-link-http';
+// import fetch from 'node-fetch';
 import Server from '../components/Server'
 import GraphQL from '../components/GraphQL'
+
+// import 'isomorphic-fetch';
+// import { parse, introspectionQuery } from 'graphql';
+// import { createApolloFetch } from 'apollo-fetch';
+
+// const apolloFetch = createApolloFetch({ uri: `http://localhost:4000/graphql` });
+// const query = parse(introspectionQuery);
 
 export default class Effects extends Component {
   constructor() {
@@ -14,16 +29,63 @@ export default class Effects extends Component {
     this.getSchema = this.getSchema.bind(this);
   }
 
+  // getSchema() {
+  //   fetchSchema('http://localhost:4000/graphql')
+  //     .then(schemaJSON => {
+  //       this.setState({
+  //         graphQLSchema: schemaJSON
+  //       })
+  //     });
+  // }
+
+  // getSchema() {
+  //   const link = new HttpLink({ uri: 'http://localhost:4000/graphql', fetch });
+
+  //     export default async() => {
+  //     const schema = await introspectSchema(link);
+      
+  //     this.setState({
+  //       graphQLSchema: schema
+  //     })
+
+  //     console.log('hello', schema);
+  //     return schema;
+  //   }
+  // }
+
+  // getSchema() {
+  //   apolloFetch({ query })
+  //     .then((result) => {
+  //       const schema = JSON.stringify(result, null, '  ');
+  //       console.log(schema);
+  //       this.setState({
+  //         graphQLSchema: schema
+  //       })
+  //     })
+  //     .catch((err) => {
+  //       console.error(err);
+  //     });
+  // }
+
   getSchema() {
-    fetchSchema('http://localhost:4000/graphql')
-      .then(schemaJSON => {
+    
+    introspectURL('http://example.com/graphql')
+      .then(result => { 
+        const schema = JSON.stringify(result, null, '  ');
+        console.log(schema);
         this.setState({
-          graphQLSchema: schemaJSON
+          graphQLSchema: schema
         })
+      })
+      .catch((err) => {
+        console.error(err);
       });
+
   }
 
   render() {
+    console.log('hello', this.state.graphQLSchema);
+
     return (
       <div>
         <Server schema={this.state.graphQLSchema} />
