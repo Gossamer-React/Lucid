dbc5b2388f3ee659b5b862d5125162ba4bdc8017
eternalee <one.eternity@gmail.com>
2018-11-15 16:49:35 -0500

    reactDevToolsGlobalHook

--- build/hook.js
+++ build/hook.js
@@ -1,6 +1,9 @@
 console.log('hook file was injected')
 
-// const reactGlobalHook = window.__REACT_DEVTOOLS_GLOBAL_HOOK__;
+const reactGlobalHook = window.__REACT_DEVTOOLS_GLOBAL_HOOK__;
+console.log('this is the reactglobalhook', reactGlobalHook);
+const getVirtualDom = const reactGlobalHook.onCommitFiberRoot;
+console.log(getVirtualDom);
 
 // reactGlobalHook.onCommitFiberRoot = (function (onCommitFiberRoot) {
 //           return function (...args) {
@@ -9,9 +12,9 @@ console.log('hook file was injected')
 //           };
 //         })(reactGlobalHook.onCommitFiberRoot)()
 
-// let x = function (z) {
-// 	return function (...args) {
-// 	  console.log(args[1]); 
-// 	  return z(...args);
-// 	};
-//     }
+let x = function (z) {
+	return function (...args) {
+	  console.log(args[1]); 
+	  return z(...args);
+	};
+    }
