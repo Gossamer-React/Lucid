2a203633bb38c74692d0f37c0fdd781d90230018
neyser zana <neyser@neysers-MacBook-Pro.local>
2018-11-15 00:42:51 -0500

    nothing change just tried moving some files around but everything went back to what it was before

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 const App = () => {
     return (
         <div>
-            <h1>Hello Neyser!!</h1>
+            <h1>Welcome to React-Lucid</h1>
         </div>
     )
 }
