990c83477a2267d0f9332a2cb9828bccc3a42b9b
eternalee <one.eternity@gmail.com>
2018-11-15 10:51:11 -0500

    backgroundscript and contentscript setup

---
+++ build/backgroundScript.js
@@ -0,0 +1,11 @@
+console.log('Background script running')
+
+chrome.browserAction.onClicked.addListener(buttonClicked);
+
+function buttonClicked(tab) {
+  // console.log(tab)
+  let msg = {
+    txt: 'hi'
+  }
+  chrome.tabs.sendMessage(tab.id, msg)
+}
