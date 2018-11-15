10883da348a53f405569ce567205b8565ebd8aea
neyser zana <neyser@neysers-MacBook-Pro.local>
2018-11-15 16:31:45 -0500

    trying to connect hooks

--- build/backgroundScript.js
+++ build/backgroundScript.js
@@ -1,11 +1,13 @@
 console.log('Background script running')
 
-chrome.browserAction.onClicked.addListener(buttonClicked);
+// chrome.browserAction.onClicked.addListener(buttonClicked);
 
-function buttonClicked(tab) {
-  // console.log(tab)
-  let msg = {
-    txt: 'hi'
-  }
-  chrome.tabs.sendMessage(tab.id, msg)
-}
+// function buttonClicked(tab) {
+//   // console.log(tab)
+//   let msg = {
+//     txt: 'hi'
+//   }
+//   chrome.tabs.sendMessage(tab.id, msg)
+// }
+
+
--- build/contentScript.js
+++ build/contentScript.js
@@ -1,12 +1,22 @@
-
 console.log("content script running!")
 
-chrome.runtime.onMessage.addListener(gotMessage);
-function gotMessage(message, sender, sendResponse) {
-  if (message.txt === 'hi') {
-    let paragraphs = document.getElementsByTagName('p');
-    for (el of paragraphs) {
-      el.style['background-color'] = '#FF0000';
-    }
-  }
-}
+// chrome.runtime.onMessage.addListener(gotMessage);
+// function gotMessage(message, sender, sendResponse) {
+//   if (message.txt === 'hi') {
+//     let paragraphs = document.getElementsByTagName('p');
+//     for (el of paragraphs) {
+//       el.style['background-color'] = '#FF0000';
+//     }
+//   }
+// }
+
+// var G_reactLucidRunning; 
+
+// if(!G_reactLucidRunning){
+//   if(!window._REACT_DEVTOOLS_GLOBAL_HOOK_) console.warn('React Dev Tools are required to be installed');
+
+//   console.log(window);
+//   const rInstance = window._REACT_DEVTOOLS_GLOBAL_HOOK_;
+
+//   console.log(rInstance);
+// }
