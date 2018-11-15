e0cd552989f947108c62ef6968a80aa2199eaed0
neyser zana <neyser@neysers-MacBook-Pro.local>
2018-11-14 23:56:17 -0500

    moving files to get chrome devtools to display our react application and testing to see if it updates

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 const App = () => {
     return (
         <div>
-            <h1>Hello Gossamer</h1>
+            <h1>Hello Neyser!!</h1>
         </div>
     )
 }
