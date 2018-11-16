d6694adfe76e208bf57a8811df98cca2c9af4e98
Nian Liu <nianliu@Nians-MacBook-Pro.local>
2018-11-15 19:07:02 -0500

    excluded node modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import LogContainer from './containers/LogContainer.jsx';
 import styles from './../public/app.css';
+import TreeDiagram from './components/TreeDiagram.jsx';
 
 const App = () => {
     return (
         <div id="app-container">
             <LogContainer />
             <h1>Welcome to React-Lucid</h1>
+            <TreeDiagram />
         </div>
     )
 }
