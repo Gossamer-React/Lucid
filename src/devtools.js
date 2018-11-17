8a22281612884cbfe7f318584a417e228d38e545
neyser zana <neyser@neysers-MacBook-Pro.local>
2018-11-17 17:52:02 -0500

    google reconfig completed

--- src/devtools.js
+++ src/devtools.js
@@ -7,6 +7,7 @@ import TreeDiagram from './components/TreeDiagram.jsx';
 
 class App extends Component {
   constructor() {
+    super();
     this.state = {
       state: 'HELLO'
     };
@@ -16,6 +17,42 @@ class App extends Component {
       'devtools.html',
       () => {
         console.log('Panel was created!!');
+
+        // chrome.runtime.onMessage.addListener(function(response, sender, sendResponse) {
+        //   alert('devtools:', response)
+        // })
+
+        //const port = chrome.extension.connect({ name: 'knockknock' });
+        // chrome.runtime.onConnect.addListener(function(port) {;
+        //   alert('portname:', port.name);
+        //   port.onMessage.addListener(function(msg) {
+        //     console.log('devtools-listener activated')
+        //     if (msg.joke == "Knock knock") {
+        //       alert('knock knock received')
+        //       port.postMessage({question: "Who's there?"});
+        //       alert('asked whosthere?')
+        //     }
+        //     else if (msg.answer == "Madame")
+        //     port.postMessage({question: "Madame who?"});
+        //     else if (msg.answer == "Madame... Bovary")
+        //     port.postMessage({question: "I don't get it."});
+        //   });
+        // });
+        // port.postMessage({
+        //   name: 'connect',
+        //   tabId: chrome.devtools.inspectedWindow.tabId,
+        // });
+        // port.onMessage.addListener((msg) => {
+        //   if (!msg.data) return; // abort if data not present, or if not of type object
+        //   if (typeof msg !== 'object') return;
+        //   if(JSON.stringify(curData) !== JSON.stringify(msg)) {
+        //     curData = msg;
+        //     logMode = false;
+        //     clearInterval(this.update);
+        //     this.update = 0;    
+        //     this.update = setInterval( () => this.updateTree(), 100);
+        //   }
+        // });
       }
     );
   }
