f7c5b841adcdc5cfd0768a3a8663481103a7d3a8
neyser zana <neyser@neysers-MacBook-Pro.local>
2018-11-17 12:00:40 -0500

    to be able to oull from master

--- src/containers/LogContainer.jsx
+++ src/containers/LogContainer.jsx
@@ -5,22 +5,14 @@ import styles from '../../public/log-container.css';
 class LogContainer extends Component {
   constructor(props) { 
     super(props);
-    this.state = {
-      logs: _Logs
-    }
   }
 
   render() {
-    console.log("LOGS:", _Logs);
-    let logs = this.state.logs.map((log) => {
-      console.log(log);
-      return  <Log />;
-    });
 
     return (
       <div id="log-container">
         <h1>Log</h1>
-        {logs}
+        <Log />;
       </div>
     )
   }
