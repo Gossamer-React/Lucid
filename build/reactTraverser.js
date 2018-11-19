12d2f8c5df3d4f69e90f32d30e053dc9951275fa
eternalee <one.eternity@gmail.com>
2018-11-19 18:03:03 -0500

    change reactTraverser from Object to Array for react-D3-Tree

--- build/reactTraverser.js
+++ build/reactTraverser.js
@@ -2,78 +2,77 @@ const reactGlobalHook = window.__REACT_DEVTOOLS_GLOBAL_HOOK__;
 
 if (reactGlobalHook) {
   let virtualdom;
+  var reactDOMArr = [];
 
   reactGlobalHook.onCommitFiberRoot = (function (oCFR) {
     return function (...args) {
       virtualdom = args[1];
       let nodeToTraverse = virtualdom.current.stateNode.current;
       traverse(nodeToTraverse);
-      console.log('traverse complete: ', documentObj);
+      console.log('traverse complete: ', reactDOMArr);
 
-      //send traverse docObj data to contentScriptJS
+      //send DOM's react component tree to contentScriptJS
       window.postMessage(JSON.parse(JSON.stringify(
-        { data: documentObj, type: 'reactTraverser'}
+        { type: 'reactTraverser', data: reactDOMArr}
       )), '*')
+      
+      reactDOMArr = [];
 
       // //get signal from contentScriptto traverse
       // window.addEventListener('traverse', () => {
       //   traverse(nodeToTraverse);
       // });
-  
+      
       return oCFR(...args);
     };
   })(reactGlobalHook.onCommitFiberRoot);
 
-  var documentObj = {};
-  const traverse = (node, childrenarr = documentObj, sib = false) => {
+  const traverse = (node, childrenarr = reactDOMArr, sib = false) => {
 
     if (node.type) {
       if (node.type.name) {
-        //console.log('********',node.type.name)
-        //if desired node, create obj and push into documentObj
+        //if desired node, create obj and push into reactDOMArr
         obj = {
-          Component: node.type.name,
-          State: node.memoizedState,
-          Children: [],
-          Id: node._debugID,
-          Props: function () {
-            try {
-              let result = {};
-              const props = node.memoizedProps;
-              if (typeof props === 'object') {
-                for (let prop in props) {
-                  const val = props[prop];
-                  if (typeof val === 'function') {
-                    //result[prop] = parseFuncName(val);
-                    result[prop] = JSON.stringify(val);
-                  } else if (typeof val === 'object') {
-                    result[prop] = JSON.stringify(val);
-                  } else {
-                    result[prop] = val;
+          name: node.type.name,
+          attributes: {
+            State: node.memoizedState,
+            Id: node._debugID,
+            Props: function () {
+              try {
+                let result = {};
+                const props = node.memoizedProps;
+                if (typeof props === 'object') {
+                  for (let prop in props) {
+                    const val = props[prop];
+                    if (typeof val === 'function') {
+                      //result[prop] = parseFuncName(val);
+                      result[prop] = JSON.stringify(val);
+                    } else if (typeof val === 'object') {
+                      result[prop] = JSON.stringify(val);
+                    } else {
+                      result[prop] = val;
+                    }
                   }
+                } else {
+                  result = props;
                 }
-              } else {
-                result = props;
+                return result;
+              } catch (e) {
+                return {};
               }
-              return result;
-            } catch (e) {
-              return {};
-            }
-          }()
+            }()
+          },
+          children: [],
         }
-        //Create parent node in documentObj
-        if (Object.keys(documentObj).length === 0) {
-          documentObj[node.type.name] = obj;
-          childrenarr = documentObj[node.type.name]['Children'];
+        //Create parent node in reactDOMArr
+        if (reactDOMArr.length === 0) {
+          reactDOMArr.push(obj);
+          childrenarr = reactDOMArr[reactDOMArr.length-1]['children']
         } else {
-          //For all other nodes, push obj into children array
-          let childObj = {};
-          childObj[node.type.name] = obj;
-          if (Array.isArray(childrenarr)) {
-            childrenarr.push(childObj);
-          }
+
+          childrenarr.push(obj)
           if (!sib) {
-            childrenarr = obj['Children'];
+            childrenarr = obj['children']
           }
         }
       }
