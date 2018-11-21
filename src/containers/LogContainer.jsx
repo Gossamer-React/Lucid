bd104b5575d838d1bf1e67be34436ff77c08348b
neyser zana <neyser@neysers-MBP.lan1>
2018-11-21 00:02:52 -0500

    request and response are now received and the state is updated

--- src/containers/LogContainer.jsx
+++ src/containers/LogContainer.jsx
@@ -1,25 +1,28 @@
-import React, { Component } from 'react';
-import Log from './../components/Log/Log.jsx';
-import styles from '../../public/log-container.css';
+import React, { Component } from "react";
+import Log from "./../components/Log/Log.jsx";
+import styles from "../../public/log-container.css";
 
-const LogContainer = (props) => {
-  console.log('LOGS: ', props);
-  let logs = props.logs.map((log, i) => {
-    return <Log key={i} operationName={log.operationName} query={log.query} variables={log.variables} logId={i} />
-  });
+const LogContainer = props => {
+  // let logs = props.logs.map((log, i) => {
+  //   return (
+  //     <Log
+  //       key={i}
+  //       operationName={log.operationName}
+  //       query={log.query}
+  //       variables={log.variables}
+  //       logId={i}
+  //     />
+  //   );
+  // });
 
   logs.reverse();
-
-  console.log('LOGS', logs);
   return (
-    <div id='log-container'>
+    <div id="log-container">
       <h1>Log</h1>
-      <hr id='log-hr' />
-      <div id='logs'>
-        {logs}
-      </div>
+      <hr id="log-hr" />
+      {/* <div id="logs">{logs}</div> */}
     </div>
-  )
-}
+  );
+};
 
 export default LogContainer;
