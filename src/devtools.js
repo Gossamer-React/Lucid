c498b1340fce883370beed5d43de3f0f5b2a7958
eternalee <one.eternity@gmail.com>
2018-11-20 20:29:18 -0500

    added react-loader check

--- src/devtools.js
+++ src/devtools.js
@@ -23,11 +23,12 @@ class App extends Component {
         let state = this;
         const backgroundPort = chrome.runtime.connect({ name: 'devtool-background-port' });
 
-        // sends a 'connect' message to backgroundScript to trigger reactTraverse
+        // send a 'connect' message to backgroundScript to trigger reactTraverse with the tabId
         backgroundPort.postMessage({
           name: 'connect',
           tabId: chrome.devtools.inspectedWindow.tabId
         })
+
 
         // *adds a listener to listen for any messages being sent by our background script
         backgroundPort.onMessage.addListener((req) => {
@@ -49,16 +50,29 @@ class App extends Component {
   }
 
   render() {
-    return (
-      <div id="app-container">
-        <LogContainer logs={this.state.logs} />
-        <h1>Welcome to React-Lucid</h1>
-        <Effects logs={this.state.logs} />
-        <TreeDiagram
-          appState={this.state.appState}
-        />
-      </div>
-    );
+    console.log('this is the states Appstate:', this.state.appState)
+    //if this.state.appState has not been populated by the reactTraverser.js
+    if (this.state.appState.length===0) {
+      //show a message that asks users to 'setState'
+      return (
+        <div id='reactLoader'>
+        <h1>Please trigger a setState() to activate Lucid devtool.<br /></h1>
+        <p>Note: Lucid works best on React v15/16</p>
+        </div>
+      )
+    } else {
+      return (
+        <div id="app-container">
+          <LogContainer logs={this.state.logs} />
+          <h1>Welcome to React-Lucid</h1>
+          <Effects logs={this.state.logs} />
+          <TreeDiagram
+            appState={this.state.appState}
+          />
+        </div>
+      );
+    }
+      
   }
 }
 
