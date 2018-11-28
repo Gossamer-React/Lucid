08e7795aa87062b28c0a4fd4c14dc688ec38cc53
yongnicholaskim <yongnicholaskim@gmail.com>
2018-11-28 15:37:04 -0500

    sdl and json formatting

--- src/containers/StateContainer.jsx
+++ src/containers/StateContainer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactJson from 'react-json-view';
 import styles from '../../public/stateContainer.css';
 
 const StateContainer = ({ stateDiffs }) => {
@@ -6,6 +7,7 @@ const StateContainer = ({ stateDiffs }) => {
   return (
     <div id='state-container'>
       <pre>{JSON.stringify(stateDiffs, null, 2)}</pre>
+      {/* <ReactJson src={stateDiffs} /> */}
     </div>
   )
 }
