4f71c84b45ca4effd6c89af46129d6cfa470f462
eternalee <one.eternity@gmail.com>
2018-11-28 23:42:44 -0500

    filter reactDOM

---
+++ src/filterComponents.js
@@ -0,0 +1,34 @@
+let filterComponents = {
+  //filter react-router
+  reactRouterComponents: [
+    'BrowserRouter',
+    'Router',
+    'Switch',
+    'Route',
+    'Link',
+    'StaticRouter',
+    'NavLink',
+    'Redirect',
+    'MemoryRouter',
+    'Prompt',
+    'NavLink',
+  ],
+
+  //filter react-redux
+  reduxComponents: ['Provider', 'Connect'],
+
+  //filter apollo-react
+  apolloComponents: [
+    'ApolloProvider',
+    'ApolloConsumer',
+    'Query',
+    'Mutation',
+    'Subscription',
+    'MockedProvider',
+    'graphql',
+    'compose',
+    'withApollo'
+  ]
+}
+
+export default filterComponents;
