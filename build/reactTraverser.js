ce2a703205e72084375d21a90d81c775caee745f
neyser zana <neyserj.zana@gmail.com>
2019-01-23 22:39:29 -0500

    editing code for easier read of traverse function

--- build/reactTraverser.js
+++ build/reactTraverser.js
@@ -16,23 +16,24 @@ if (reactGlobalHook) {
     if (reactInstance && reactInstance.version) {
       reactGlobalHook.onCommitFiberRoot = (function (oCFR) {
         return function (...args) {
-          
+
           if (args[1] !== undefined) {
             clearTimeout(timeout);
             timeout = setTimeout(() => {
               virtualdom = args[1];
               let nodeToTraverse = virtualdom.current.stateNode.current;
               traverse(nodeToTraverse);
-  
+
+              console.log(virtualdom, reactDOMArr);
               //send DOM's react component tree to contentScriptJS
               window.postMessage(JSON.parse(JSON.stringify({
                 type: 'reactTraverser',
                 data: reactDOMArr
-              })), '*')
-  
+              })), '*');
+
               reactDOMArr = [];
             }, 750);
-            
+
             return oCFR(...args);
           }
         };
@@ -45,62 +46,64 @@ if (reactGlobalHook) {
     }
   }
   setHook();
-  const traverse = (node, childrenarr = reactDOMArr, sib = false) => {
-    if (node.type) {
-      if (node.type.name) {
-        //if desired node, create obj and push into reactDOMArr
-        obj = {
-          name: node.type.name,
-          attributes: {
-            Id: node._debugID
-          },
-          children: [],
-          State: node.memoizedState,
-          Props: function () {
-            try {
-              let result = {};
-              const props = node.memoizedProps;
-              if (typeof props === 'object') {
-                for (let key in props) {
-                  const val = props[key];
-                  if (typeof val === 'function' || typeof val === 'object') {
-                    result[key] = JSON.stringify(val);
-                  } else {
-                    result[key] = val;
-                  }
-                }
-              } else {
-                result = props;
-              }
-              return result;
-            } catch (e) {
-              return {};
-            }
-          }()
-        }
 
-        //Create parent node in reactDOMArr
-        if (reactDOMArr.length === 0) {
-          reactDOMArr.push(obj);
-          childrenarr = reactDOMArr[reactDOMArr.length - 1]['children']
-        } else {
-          childrenarr.push(obj)
-          if (!sib) {
-            childrenarr = obj['children']
+} else {
+  console.log('React devtool is required to use React-Lucid');
+}
+
+const traverse = (node, childrenarr = reactDOMArr, sib = false) => {
+  if (node.type) {
+    if (node.type.name) {
+      //if desired node, create obj and push into reactDOMArr
+      obj = {
+        name: node.type.name,
+        attributes: {
+          Id: node._debugID
+        },
+        children: [],
+        State: node.memoizedState,
+        Props: function () {
+          try {
+            let result = {};
+            const props = node.memoizedProps;
+            if (typeof props === 'object') {
+              for (let key in props) {
+                const val = props[key];
+                if (typeof val === 'function' || typeof val === 'object') {
+                  result[key] = JSON.stringify(val);
+                } else {
+                  result[key] = val;
+                }
+              }
+            } else {
+              result = props;
+            }
+            return result;
+          } catch (e) {
+            return {};
           }
+        }()
+      }
+
+      //Create parent node in reactDOMArr
+      if (reactDOMArr.length === 0) {
+        reactDOMArr.push(obj);
+        childrenarr = reactDOMArr[reactDOMArr.length - 1]['children']
+      } else {
+        childrenarr.push(obj)
+        if (!sib) {
+          childrenarr = obj['children']
         }
       }
     }
+  }
 
-    if (node.child !== null) {
-      traverse(node.child, childrenarr, false);
-    }
-    if (node.sibling) {
-      traverse(node.sibling, childrenarr, true);
-    }
-    return;
+  if (node.child !== null) {
+    traverse(node.child, childrenarr, false);
+  }
+  if (node.sibling) {
+    traverse(node.sibling, childrenarr, true);
+  }
+  return;
 
-  };
-} else {
-  console.log('React devtool is required to use React-Lucid')
-}
+};
