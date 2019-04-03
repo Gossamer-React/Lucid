fe7e7f3bf8ef853d7f002947c5fe8a0bf14ac04c
neyser zana <neyserj.zana@gmail.com>
2019-04-02 22:39:35 -0400

    changing webpack config for chunking and optimizing build

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
 const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   entry: './src/devtools.js',
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: 'webpack-bundle.js'
+    filename: 'webpack-bundle.js',
+    publicPath: ''
   },
   module: {
     rules: [
@@ -15,23 +20,50 @@ module.exports = {
           {
             loader: 'babel-loader',
             options: {
-              presets: ['@babel/react', ['@babel/env', {modules: false}]]
+              presets: ['@babel/react', ['@babel/env', { modules: false }]]
             }
           }
         ]
       },
       {
         test: /\.css$/,
+        exclude: /node_modules/,
         use: [
           'style-loader',
-          'css-loader'
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+              importLoaders: 1,
+              modules: true,
+              localIdentName: '[name]_[local]_[hash:base64:5]'
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: () => [
+                autoprefixer({
+                  browsers: ['last 2 versions']
+                })
+              ]
+            }
+          }
         ]
       }
     ]
   },
-  devServer: {
-    contentBase: path.resolve(__dirname, '.'),
-    port: 3001,
-    open: true,
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'devtools.html',
+      template: 'devtools.html',
+      chunksSortMode: 'dependency',
+      inject: 'body'
+    })
+  ],
+  optimization: {
+    minimizer: [new TerserPlugin()],
+    nodeEnv: 'production'
   }
 }
