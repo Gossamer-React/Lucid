8fb6f40dfc90108f5586d7afcd01c9dbce5deeab
neyser zana <neyserj.zana@gmail.com>
2019-04-04 15:14:42 -0400

    changing around webpack config for better proudction bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const autoprefixer = require('autoprefixer');
 const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
@@ -27,29 +26,9 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        exclude: /node_modules/,
         use: [
           'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: true,
-              importLoaders: 1,
-              modules: true,
-              localIdentName: '[name]_[local]_[hash:base64:5]'
-            }
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              ident: 'postcss',
-              plugins: () => [
-                autoprefixer({
-                  browsers: ['last 2 versions']
-                })
-              ]
-            }
-          }
+          'css-loader'
         ]
       }
     ]
@@ -59,7 +38,7 @@ module.exports = {
       filename: 'devtools.html',
       template: 'devtools.html',
       chunksSortMode: 'dependency',
-      inject: 'body'
+      inject: 'head'
     })
   ],
   optimization: {
