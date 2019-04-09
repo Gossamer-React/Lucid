be6e48853b4dfcd10520218fc2f5435744e75de8
neyser zana <neyserj.zana@gmail.com>
2019-04-08 21:13:50 -0400

    moving components into their own folders with their styles to organize code base

---
+++ src/components/GraphQLSchema/GraphQLSchema.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { buildClientSchema, printSchema } from 'graphql';
+import { GraphqlCodeBlock } from 'graphql-syntax-highlighter-react';
+import styles from './GraphQLSchema.css';
+
+const GraphQLSchema = ({ schema }) => {
+
+  if (schema !== 'GraphQL schema not available.') {
+
+    let schemaObj = buildClientSchema(JSON.parse(schema));
+    let schemaSDL = printSchema(schemaObj);
+
+    return (
+      <div id='graphql-schema'>
+        <div>
+          <h2 className='graphql-heading'>Schema:</h2>
+          <span className='graphql-span'>
+            <GraphqlCodeBlock
+              className='GraphqlCodeBlock'
+              queryBody={schemaSDL}
+            />
+          </span>
+        </div>
+      </div>
+    )
+
+  } else {
+
+    return (
+      <div id='graphql'>
+        <div>
+          <p className='graphql-p'>
+            No GraphQL data available.
+          </p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default GraphQLSchema;
