7f3c9dc4d61c2c2ae14ebe165eb5006181af0f8d
neyser zana <neyserj.zana@gmail.com>
2019-04-08 21:14:30 -0400

    refactoring component and removing unnecssary styles

---
+++ src/containers/GraphQLTab/GraphQLTab.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import LogContainer from './../LogContainer.jsx';
+import GraphQLResponse from '../../components/GraphQLResponse/GraphQLResponse';
+import GraphQLSchema from '../../components/GraphQLSchema/GraphQLSchema';
+import styles from './GraphQLTab.css';
+
+const tabChange = (prevProps, nextProps) => {
+  if (nextProps.tab !== 'Graphql') return true;
+
+  return false;
+};
+
+const GraphQLTab = props => {
+  return (
+    <div id='graphQLTab'>
+      <LogContainer
+        logs={props.logs}
+        clearLog={props.clearLog}
+        logChange={props.logChange}
+      />
+      
+      {/* Checks to see it there was a request made. */}
+      {props.log ? (
+        <div id='graphql-container'>
+          <GraphQLResponse log={props.log} />
+          <GraphQLSchema schema={props.schema} />
+        </div>
+      ) : (
+          <h2>No requests have been made yet.</h2>
+        )}
+    </div>
+  );
+};
+
+export default React.memo(GraphQLTab, tabChange);
