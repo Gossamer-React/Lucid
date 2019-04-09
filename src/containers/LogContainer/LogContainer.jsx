be5673c58b068b219a48ceb28bd592d4b36506d9
neyser zana <neyserj.zana@gmail.com>
2019-04-08 23:47:36 -0400

    moving components into their own folders with their corresponding css files

---
+++ src/containers/LogContainer/LogContainer.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import Log from './../../components/LogComponent/Log.jsx';
+import styles from './LogContainer.css';
+
+const lengthsAreEqual = (prevProps, nextProps) => {
+  if (prevProps.logs.length === nextProps.logs.length) return true;
+
+  return false;
+};
+
+const LogContainer = props => {
+  let logs = props.logs.map((log, i) => {
+    let text = JSON.parse(log.req.postData.text);
+    return (
+      <Log
+        key={i}
+        operationName={text.operationName}
+        query={text.query}
+        variables={text.variables}
+        logId={i}
+        logChange={props.logChange}
+      />
+    );
+  });
+
+  logs.reverse();
+
+  return (
+    <React.Fragment>
+      {logs.length > 0 ? (
+        <div id='log-container'>
+          <div id='log-header'>
+            <h2>Request Log</h2>
+            <button
+              className='appButton'
+              data-log='req-log'
+              onClick={e => {
+                props.clearLog(e);
+              }}
+            >
+              Clear
+            </button>
+          </div>
+          <div id='logs'>{logs}</div>
+        </div>
+      ) : null}
+    </React.Fragment>
+  );
+};
+
+export default React.memo(LogContainer, lengthsAreEqual);
